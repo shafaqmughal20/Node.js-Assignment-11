6612254cbafe28c70ac1f64054e1331b1fcfcc5a
shafaqmughal20 <160257641+shafaqmughal20@users.noreply.github.com>
2024-03-03 07:31:47 +0500

    Add files via upload

---
+++ Q10.js
@@ -0,0 +1,6 @@
+"use strict";
+//10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// date:2.23.2024;
+//{ solving question no 10 }
+let favoritNum = (4);
+console.log(`number ${favoritNum} is my favorite number`);
---
+++ Q10.ts
@@ -0,0 +1,8 @@
+//10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+
+// date:2.23.2024;
+//{ solving question no 10 }
+
+let favoritNum:number=(4)
+console.log (`number ${favoritNum} is my favorite number`)
